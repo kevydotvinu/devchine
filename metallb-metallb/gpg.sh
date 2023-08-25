b66b6a0c38700e0cb25dbc9249f3f3ed0675a622
Vinu K <kevy.vinu@gmail.com>
2023-08-25 12:58:12 +0530

    refactor: Modify gpg script
    - Execute script if the parameter does not exist

--- metallb-metallb/gpg.sh
+++ metallb-metallb/gpg.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # This script edit sshd_config and restart sshd
 
-echo "StreamLocalBindUnlink yes" | sudo tee -a /etc/ssh/sshd_config
-sudo systemctl restart sshd
+if ! sudo grep 'StreamLocalBindUnlink' /etc/ssh/sshd_config; then
+	echo "StreamLocalBindUnlink yes" | sudo tee -a /etc/ssh/sshd_config
+	sudo systemctl restart sshd
+fi
